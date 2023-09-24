2862f31f02ec6843e3042341d3894b7fe895383c
arnaav11 <87122819+arnaav11@users.noreply.github.com>
2023-09-24 12:50:00 +0530

    Update users.py

--- Arnav/users.py
+++ Arnav/users.py
@@ -4,6 +4,17 @@ import otp
 
 def register(uname: str, passwd: str, email: str, connection: pymysql.Connection, cursor: pymysql.cursors.Cursor):
 
+
+    '''
+    
+    Registers user in the database
+
+    Returns `True` if successful
+
+    Returns `False` if user already exists
+    
+    '''
+
     cursor.execute(f'select username from users where username="{uname}"')
     data = cursor.fetchall()
 
@@ -25,7 +36,16 @@ def register(uname: str, passwd: str, email: str, connection: pymysql.Connection
     
 def login(uname: str, passwd: str, connection: pymysql.Connection, cursor: pymysql.cursors.Cursor):
 
-    cursor.execute(f'select username from users where username="{uname}"')
+
+    '''
+    
+    Checks login credentials from the database
+
+    Returns `True` only if credentials are correct
+    
+    '''
+
+    cursor.execute(f'select * from users where username="{uname}"')
 
     if cursor.fetchall() == ():
 
@@ -43,7 +63,17 @@ def login(uname: str, passwd: str, connection: pymysql.Connection, cursor: pymys
 
 def forgot_passwd(email: str, connection: pymysql.Connection, cursor: pymysql.cursors.Cursor):
 
-    cursor.execute(f'select username from users where email="{email}"')
+    '''
+    
+    Sends `otp` to `email` for password recovery
+
+    returns `otp` if `email` exists in DB and can be sent to.
+
+    else returns `-1`
+    
+    '''
+
+    cursor.execute(f'select * from users where email="{email}"')
     data = cursor.fetchall()
 
     if data == ():
@@ -51,4 +81,4 @@ def forgot_passwd(email: str, connection: pymysql.Connection, cursor: pymysql.cu
     
     else:
 
-        return otp.send_otp(data[4])
+        return otp.send_otp(data[0][4])
