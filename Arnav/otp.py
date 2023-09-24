3b60cbab849defc398ff367aff8252b05237ac6f
arnaav11 <87122819+arnaav11@users.noreply.github.com>
2023-09-24 12:49:34 +0530

    Update otp.py

--- Arnav/otp.py
+++ Arnav/otp.py
@@ -1,24 +1,37 @@
 import smtplib
 from email.message import EmailMessage
 import random
+from validate_email import validate_email
 
 def send_otp(email: str):
 
-    sender = "lakhya.arnav.cs.project@gmail.com"
-
-    otp = random.randint(100000, 999999)
-    message = EmailMessage()
-    message.set_content(f'The OTP for Alpha Bravo password reset is: {otp}')
+    '''
     
-    message['Subject'] = f'Alpha-Bravo Password Recovery [{otp}]'
-    message['From'] = sender
-    message['To'] = email
+    Sends a random 6-digit `OTP` to `email`
 
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.login(sender, "tkbw uufq ziyx smnq")
+    Returns `OTP` if email is valid, else returns `-1`
     
+    '''
 
-    server.send_message(message)
-    server.quit()
+    if validate_email(email):
 
-    return otp
+        sender = "lakhya.arnav.cs.project@gmail.com"
+
+        otp = random.randint(100000, 999999)
+        message = EmailMessage()
+        message.set_content(f'The OTP for Alpha Bravo password reset is: {otp}')
+        
+        message['Subject'] = f'Alpha-Bravo Password Recovery [{otp}]'
+        message['From'] = sender
+        message['To'] = email
+
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.login(sender, "tkbw uufq ziyx smnq")
+        
+
+        server.send_message(message)
+        server.quit()
+
+        return otp
+    
+    return -1
