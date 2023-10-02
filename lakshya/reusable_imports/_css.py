3b9043932df2519598a7e66eb88f1460c6d960d0
Lakshya Saxena <lakshya.saxena076@gmail.com>
2023-10-03 01:29:55 +0530

    Add files via upload
    Added the first 3 windows and their connection with main.py

---
+++ lakshya/reusable_imports/_css.py
@@ -0,0 +1,36 @@
+checklist_frame_selection_css = """
+border-radius:5px; 
+border-color:black black black black; 
+background-color: #F0E7FF; 
+selection-background-color: black; 
+border-width: 2px; 
+border-style: solid; 
+border-color: rgb(9, 23, 217) rgb(9, 23, 217) rgb(9, 23, 217) rgb(9, 23, 217);
+"""
+
+checklist_image_selection_css = """
+border-width:0px;
+background-color:#F0E7FF;
+"""
+
+checklist_title_selection_css = """
+border-width:0px;
+background-color:#F0E7FF;
+"""
+
+genre_frame_selection_css = """
+border-radius:5px; 
+border-color:black black black black; 
+background-color: #F0E7FF; 
+selection-background-color: black; 
+border-width: 2px; 
+border-style: solid; 
+border-color: rgb(9, 23, 217) rgb(9, 23, 217) rgb(9, 23, 217) rgb(9, 23, 217);
+font:18pt \"MS Shell Dlg 2\";
+"""
+
+genre_title_selection_css = """
+border-width:0px;
+background-color:#F0E7FF;
+font:18pt \"MS Shell Dlg 2\";
+"""
