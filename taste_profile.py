46e13126af43bc2048dcd6858044d6d61e3f7b44
Lakshya Saxena <lakshya.saxena076@gmail.com>
2023-11-08 13:00:48 +0530

    enhancements; changing css
    Signed-off-by: Lakshya Saxena <lakshya.saxena076@gmail.com>

--- taste_profile.py
+++ taste_profile.py
@@ -1,10 +1,10 @@
 import sys
 
-from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QDialog, QApplication
 from PyQt5.uic import loadUi
 
 from reusable_imports._css import dark_main_stylesheet, dark_mainwin_widget
+from display_movie import DisplayMovies
 
 
 class TasteProfile(QDialog):
@@ -19,8 +19,29 @@ class TasteProfile(QDialog):
         self.taste_home.setStyleSheet("background-color:#313a46; border:none;")
         self.taste_search.setStyleSheet("background-color:#313a46; border:none;")
 
-        self.watched.setStyleSheet(dark_mainwin_widget)
-        self.search.setStyleSheet(dark_mainwin_widget)
+        self.watched.setStyleSheet("font:14pt;")
+        self.search.setStyleSheet("font:14pt;")
+
+        self.taste_home.clicked.connect(self.home_func)
+        self.taste_search.clicked.connect(self.search_func)
+
+    def home_func(self):
+        """
+        Function to switch to the home widget in the stack
+        """
+        self.stack.setCurrentIndex(0)
+        if self.expand.isVisible():
+            self.expand.hide()
+            self.collapse.show()
+
+    def search_func(self):
+        """
+        Function to switch to the search widget in the stack
+        """
+        self.stack.setCurrentIndex(1)
+        if self.expand.isVisible():
+            self.expand.hide()
+            self.collapse.show()
 
 
 if __name__ == '__main__':
