7f9a011c12361cbc0d7a6e62195b4d0a723bdc78
Arnav Pushkarna <kakamishu2012@gmail.com>
2024-07-05 16:43:42 +0530

    removing api key

--- backend/movie_search.py
+++ backend/movie_search.py
@@ -1,7 +1,5 @@
 import tmdbsimple as tmdb
 
-tmdb.API_KEY = '10af8f144bffa38b9fc3a0ced21b52af'
-
 def search(phrase: str, search: tmdb.Search) -> list[dict]:
     """
     search for movies using `phrase`
